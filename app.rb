c5a4edf5f103dd7791e2fb23eb070936fa88f5b8
alexandrospapagiannis <alpa31291@gmail.com>
2022-05-04 17:46:23 +0300

    Step 12, params

--- app.rb
+++ app.rb
@@ -14,7 +14,13 @@ get '/response' do
     "my leg hurts."
   end
 
-get '/cat' do
+get '/random-cat' do
     @name = ["Amigo", "Misty", "Almond"].sample
+    erb(:index)
+  end
+
+  get '/named-cat' do
+    p params
+    @name = params[:name]
     erb(:index)
   end
